06a6574f7ecc66d84c3a6b2fde4a8eae4ed1118c
oalmhage <142837730+oalmhage@users.noreply.github.com>
2024-02-06 15:31:22 +0100

    Some changes with imagePerPage and some changes in CSS fonts and more

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let apiKey = '42149832-f9141c5344ccf49924bc7a124';
 let currentPage = 1;
-let imagesPerPage = 10;
+let imagesPerPage = 12; //Godkänt av Jakob
 let searchTerm = "";
 let colorFilter = "";
 let previousSearchTerm = "";
